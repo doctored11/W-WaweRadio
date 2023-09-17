fb675f73449f24c12a96ab6eb04a6ebca20743b5
doctored11 <edit.shultz@yandex.ru>
2023-09-17 23:55:08 +0300

    first grid

--- res/scripts/script.js
+++ res/scripts/script.js
@@ -1,10 +1,16 @@
 
 const podcastCards = document.querySelectorAll('.podcast-card');
 const showsCard = document.querySelectorAll('.shows__card')
+const playCards = document.querySelectorAll('.card--playlists');
+const flags = document.querySelectorAll('.card__absolute-logo--flag');
+const peoplesCards = document.querySelectorAll('.card--about');
 
 addBackImages(podcastCards);
 
 addBackImages(showsCard);
+addBackImages(playCards);
+addBackImages(flags);
+addBackImages(peoplesCards);
 function addBackImages(target) {
     target.forEach(function (card) {
 
